f359b84a907c16b4200be92095b6757395d1e060
nevik <bartlamkevin@gmail.com>
2022-02-15 19:23:00 -0500

    files updated

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 --Given this function:
 
 function filterOutOdds() {
-  var nums = Array.prototype.slice.call(arguments);
-  return nums.filter(function(num) {
-    return num % 2 === 0
-  });
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+        return num % 2 === 0
+    });
 }
 
 --Refactor it to use the rest operator & an arrow function:
@@ -15,7 +15,7 @@ function filterOutOdds() {
 const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
 
 
-/* 
+/* findMin
 --Write a function called findMin that accepts a variable number of arguments 
 --and returns the smallest argument.
 
@@ -27,45 +27,95 @@ findMin(3,1) // 1
 */
 
 const findMin = (...nums) => Math.min(...nums);
+
 /* 
---Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+--Write a function called mergeObjects that accepts two objects
+and returns a new object which contains all the keys and values 
+of the first object and second object.
 
 mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 */
-
-
+//uses spread
+const mergeObjects = (obj1, obj2) => ({...obj1,...obj2});
 
 /* 
---Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+--Write a function called doubleAndReturnArgs which accepts an array 
+--and a variable number of arguments. The function should return a new array 
+--with the original array values and all of additional arguments doubled.
 
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 */
 
-
-
+const doubleAndReturnArgs = (arr, ...nums) => [...arr, ...nums.map(num => num *2)];
 
 /* Slice and Dice! 
+write these six  functions using: 
+-rest
+-spread
+-refactor to be arrow functions
+
+1.
 --remove a random element in the items array 
 --and return a new array without that item. 
-
+s
 function removeRandom(items) {}
+*/
+const removeRandom = itemsArr =>{
+    let ind = Math.floor(Math.random()*itemsArr.length);
+    return [...itemsArr.slice(0, ind), ...itemsArr.slice(ind + 1)];
+}
 
+/*
+2.
 --Return a new array with every item in array1 and array2.
 
 function extend(array1, array2) {}
+*/
 
+const extend = (array1, array2) => [...array1, ...array2];
+
+
+/* 
+3.
+--Return a new object with all the keys and values
+--from obj and a new key/value pair
+
+function addKeyVal(obj, key, val) {}
+*/
+
+const addKeyVal = (obj, key, val) => {
+    let newObj = {...obj};
+    newObj[key] = val;
+    return newObj;
+}
+/*
+4. 
 --Return a new object with a key removed.
 
 function removeKey(obj, key) {}
+*/
 
-
+const removeKey = (obj, key) =>{
+    let newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+/*
+4. 
 --Combine two objects and return a new object.
 
 function combine(obj1, obj2) {}
-
-
+*/
+const combine = (obj1, obj2) => ({...obj1, ...obj2});
+/*
+5.
 --Return a new object with a modified key and value.
 
 function update(obj, key, val) {}
 */
+const update = (obj,key,val)=>{
+    let newObj = {...obj};
+    newObj[key] = val;
+    return newObj;
+}
